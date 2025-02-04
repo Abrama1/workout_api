c8fb12325f0fe37e5b4c3e22c197219c96c6021d
Davit Abramishvili <david.abramishvili10@gmail.com>
2025-02-04 14:27:17 +0400

    Configured Django settings with JWT authentication and PostgreSQL

--- workout_api/settings.py
+++ workout_api/settings.py
@@ -37,7 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'drf_yasg',  # Swagger documentation
+    'accounts',
+    'workouts',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,10 +86,15 @@ WSGI_APPLICATION = 'workout_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'workout_db',
+        'USER': 'postgres',
+        'PASSWORD': 'your_password!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
