1b0f62097ba4b2b9f160f8914df7cc3c7ba0a0ec
Davit Abramishvili <david.abramishvili10@gmail.com>
2025-02-04 17:37:42 +0400

    Implemented API for creating and listing workout plans

--- workouts/views.py
+++ workouts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import WorkoutPlan
+from .serializers import WorkoutPlanSerializer
 
-# Create your views here.
+class WorkoutPlanListCreateView(generics.ListCreateAPIView):
+    """
+    API view for listing and creating workout plans.
+    """
+
+    serializer_class = WorkoutPlanSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Only authenticated users can create workout plans
+
+    def get_queryset(self):
+        """Return only the workout plans created by the authenticated user."""
+
+        return WorkoutPlan.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        """Asing the authenticated user to the new workout plan"""
+
+        serializer.save(user=self.request.user)
