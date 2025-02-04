779ea0b681047c2026d09b14cf487f15de3be47d
Davit Abramishvili <david.abramishvili10@gmail.com>
2025-02-04 19:28:00 +0400

    Added Swagger API documentation

--- workout_api/urls.py
+++ workout_api/urls.py
@@ -16,9 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Workout API",
+        default_version='v1',
+        description="API for managing workout plans and fitness goals",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
-    path('api/workouts/', include('workouts.urls'))
+    path('api/workouts/', include('workouts.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
