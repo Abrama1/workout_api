c6d4a29bc518d8bbf5b102a00f0bcd7b3070175a
Davit Abramishvili <david.abramishvili10@gmail.com>
2025-02-04 17:40:17 +0400

    Added URL routing for Workout Plan API

---
+++ workouts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import WorkoutPlanListCreateView
+
+urlpatterns = [
+    path('workout-plans', WorkoutPlanListCreateView.as_view(), name='workout-plans'),
+]
