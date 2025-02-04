4b3ae6070451f773274ce5124097ff5aabefb838
Davit Abramishvili <david.abramishvili10@gmail.com>
2025-02-04 19:21:07 +0400

    Added URL routing for Fitness Goals and Weight Tracking

--- workouts/urls.py
+++ workouts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import WorkoutPlanListCreateView
+from .views import WorkoutPlanListCreateView, FitnessGoalListCreateView, WeightTrackingListCreateView
 
 urlpatterns = [
     path('workout-plans/', WorkoutPlanListCreateView.as_view(), name='workout-plans'),
+    path('fitness-goals/', FitnessGoalListCreateView.as_view(), name='fitness-goals'),
+    path('weight-tracking/', WeightTrackingListCreateView(), name='weight-tracking'),
 ]
