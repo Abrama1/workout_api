68b9a3cf3500d2329a360684366f212b1ce7a8ce
Davit Abramishvili <david.abramishvili10@gmail.com>
2025-02-04 19:13:51 +0400

    Added missing forwardslashes

--- workouts/urls.py
+++ workouts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import WorkoutPlanListCreateView
 
 urlpatterns = [
-    path('workout-plans', WorkoutPlanListCreateView.as_view(), name='workout-plans'),
+    path('workout-plans/', WorkoutPlanListCreateView.as_view(), name='workout-plans'),
 ]
