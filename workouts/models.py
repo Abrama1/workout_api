60d330a1624fd1a21f9a3111a42df921600cf584
Davit Abramishvili <david.abramishvili10@gmail.com>
2025-02-04 16:45:24 +0400

    Added Exercise model for predefined exercises

--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Exercise(models.Model):
+    """Model representing a predefined exercise"""
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    target_muscle = models.CharField(max_length=100)
